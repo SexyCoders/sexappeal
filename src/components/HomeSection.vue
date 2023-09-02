e80e04e37211d578f5c6002dea53a0f9323033f2
Sotiris Anagnostopoulos <sotiris@sexycoders.org>
2023-09-02 17:33:42 +0300

    added hero section and created scroll effects and video playback

---
+++ src/components/HomeSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container fluid class="fullscreen-container">
+    <video autoplay loop muted class="fullscreen-video">
+      <source src="https://lib.sexycoders.org/share/logos/live/SexyCodersBanner.mp4" type="video/mp4">
+    </video>
+  </v-container>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'HomeSection',
+  data: () => ({
+  }),
+});
+</script>
+
+<style scoped>
+.fullscreen-container {
+  padding: 0 !important;
+  margin: 0 !important;
+  position: relative;
+  width: 100%;
+  height: 100vh;  /* Full viewport height */
+}
+
+.fullscreen-video {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;  /* Cover the entire screen without stretching */
+  z-index: 1;
+}
+</style>
+
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-text-field
+    class="custom-search-bar"
+    solo
+    rounded
+    hide-details
+    label="Search"
+    prepend-inner-icon="mdi-magnify"
+  ></v-text-field>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'SearchBar'
+});
+</script>
+
+<style scoped>
+.custom-search-bar {
+  height: 36px;
+  border-radius: 18px !important;
+  margin-top: auto;
+  margin-bottom: auto;
+}
+.custom-search-bar label {
+  font-size: 14px;
+}
+.custom-search-bar .v-input__icon {
+  margin-top: 5px;
+}
+.custom-search-bar input {
+  font-size: 14px;
+  padding-left: 10px;
+  padding-right: 10px;
+  color: white;
+}
+.custom-search-bar .v-input__slot {
+  border-bottom: none !important;
+  height: 36px;
+}
+</style>
+
